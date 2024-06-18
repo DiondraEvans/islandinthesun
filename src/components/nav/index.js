7acc23aca2ee965f25390989d5f20c4ae4b78075
Diondra Evans <deeandrm@gmail.com>
2024-06-18 18:14:23 -0400

    added email to contact, fixed css for nav buttons, and added line height to about text

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -8,9 +8,9 @@ const Nav = () => {
 return (
     <div class="Nav">
         <Link to='/#categoryList'><div>Portfolio</div></Link>
-       <Link to='/Work'> <div>Work with me</div></Link>
-       <Link to= "/#about"> <div>About</div></Link>
-        <div><a class="Contact" href="/Contact">Contact</a></div>
+       {/* <Link to='/Work'> <div>Work with me</div></Link> */}
+       <Link to= "/#about"><div>About</div></Link>
+        <a href="mailto:diondrajevans@gmail.com" class="Contact"><div>Contact</div></a>
     </div>
 )
 }
--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,22 +1,79 @@
-import React from 'react';
-import './index.css';
-import Nav from '../../components/nav';
+// import React, { useState } from 'react';
+// import './index.css';
+// import Nav from '../../components/nav';
 
+// function Contactform (){
+//   const [formData, setFormData] = useState({
+//     firstName:'', LastName:'', email:'', phone:'', message:''
+//   })
+// let handleSubmit=(e)=>{
+//   e.preventDefault();
+//   setFormData(e)
+//   console.log('Form submitted:', formData);
 
-
-const GetWork = () => {
-  
-   
-return (
-     <div> 
-      <Nav />
-      <div class="Hero-webdesign">
-          <h1>Web Strategy, Design, & Development</h1>
-          <p>Offering a complete package of services, covering online strategy, visual design, and technical implementation. Our expertise lies in crafting captivating interactive experiences that seamlessly work across various devices, providing exceptional solutions for our valued clients.</p>
-      </div>
-     </div>  
-      
-);
-}
-  
-  export default GetWork;
+// }
+// let handleChange =(e)=>{
+// // Destructure name and value from the target input element
+// const { name, value } = e.target;
+// // Update the state with the new value for the corresponding field
+// setFormData({
+//   ...formData,
+//   [name]: value
+// });
+// }
+// return(
+//   <>
+//   <Nav />
+//   <form onSubmit = {handleSubmit}>
+//     <div>
+//       <label htmlFor="firstName">First Name:</label>
+//       <input type="text" id="firstName" name="firstName" value={formData.firstName} onChange={handleChange} required />
+//     </div>
+//     <div>
+//         <label htmlFor="lastName">Last Name:</label>
+//         <input
+//           type="text"
+//           id="lastName"
+//           name="lastName"
+//           value={formData.lastName}
+//           onChange={handleChange}
+//           required
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="email">Email:</label>
+//         <input
+//           type="email"
+//           id="email"
+//           name="email"
+//           value={formData.email}
+//           onChange={handleChange}
+//           required
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="phone">Phone Number:</label>
+//         <input
+//           type="tel"
+//           id="phone"
+//           name="phone"
+//           value={formData.phone}
+//           onChange={handleChange}
+//         />
+//       </div>
+//       <div>
+//         <label htmlFor="message">Message:</label>
+//         <textarea
+//           id="message"
+//           name="message"
+//           value={formData.message}
+//           onChange={handleChange}
+//           required
+//         />
+//       </div>
+//       <button type="submit">Submit</button>
+//   </form>
+//   </>
+// )
+// }
+// export default Contactform;
