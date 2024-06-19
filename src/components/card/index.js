d1d3a1ca281aedc1ae78703dec11f6772bf6886e
Diondra Evans <deeandrm@gmail.com>
2024-06-19 09:22:57 -0400

    removing setCase from Appcontext

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -4,37 +4,38 @@ import { Link } from 'react-router-dom';
 import { AppContext } from '../../contexts/app_context';
 
 function Card(props) {
-    let {setShowDiv, showDiv, Case, setCase} = useContext(AppContext);
+    let {setShowDiv, showDiv, Case} = useContext(AppContext);
+    //setCase was pulled from AppContext above however now I am using the URLs to show my work instead of case by case page popups
    let object = props.object
 console.log(object.cardTitle)
 
     function handleClick() {
         if ( object.cardTitle === "Eat More Korean"){
-            setCase("emk")
+            // setCase("emk")
             console.log(Case)
             window.location.href="https://www.figma.com/proto/uYdFZLAoAMXMdnayE5GaYH/EMK-project?kind=proto&node-id=307-890&page-id=0%3A1&scaling=scale-down&starting-point-node-id=315%3A146&viewport=590%2C-572%2C0.04"
            
         }else if ( object.cardTitle === "BookClub"){
-            setCase("bc")
+            // setCase("bc")
             console.log(Case)
             window.location.href="https://timely-flan-0e0f4c.netlify.app/"
     
         }else if ( object.cardTitle === "WindBNB"){
-            setCase("wbnb")
+            // setCase("wbnb")
             console.log(Case)
             window.location.href = 'https://windbnb-zd7l.onrender.com';
 
         }else if ( object.cardTitle === "Sneakers"){
-            setCase("sn")
+            // setCase("sn")
             console.log(Case)
             window.location.href ='https://sneakers-c24z.onrender.com/'
         }else if ( object.cardTitle === "Alien Battle"){
-            setCase("ab")
+            // setCase("ab")
             console.log(Case)
             window.location.href ='https://diondraevans.github.io/Space-Battle-MOD-1-PROJECT-/'
             
         }else if ( object.cardTitle === "G3 Solutions"){
-            setCase("g3")
+            // setCase("g3")
             console.log(Case)
             window.location.href ='https://www.figma.com/proto/J1hy96GfumSRy89exz5Z5O/G3-Solutions?page-id=0%3A1&node-id=1-2&viewport=368%2C413%2C0.05&t=VveAOVdb7hA9QD2c-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1%3A2'
         }
